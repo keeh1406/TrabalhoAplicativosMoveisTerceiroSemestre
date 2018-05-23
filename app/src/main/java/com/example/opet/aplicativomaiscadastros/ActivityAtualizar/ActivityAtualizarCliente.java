1b745cbcb6ee53ebe9f17d90b4f37333bd5a7189
Kethllyn dos Santos Martins <30841388+keeh1406@users.noreply.github.com>
2018-05-23 08:42:00 -0300

    23/05/2018 08h41

---
+++ app/src/main/java/com/example/opet/aplicativomaiscadastros/ActivityAtualizar/ActivityAtualizarCliente.java
@@ -0,0 +1,8 @@
+package com.example.opet.aplicativomaiscadastros.ActivityAtualizar;
+
+/**
+ * Created by opet on 23/05/2018.
+ */
+
+public class ActivityAtualizarCliente {
+}
--- app/src/main/java/com/example/opet/aplicativomaiscadastros/ActivityListar/ActivityListarProdutos.java
+++ app/src/main/java/com/example/opet/aplicativomaiscadastros/ActivityListar/ActivityListarProdutos.java
@@ -1,8 +1,51 @@
 package com.example.opet.aplicativomaiscadastros.ActivityListar;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.example.opet.aplicativomaiscadastros.ActivityAtualizar.ActivityAtualizarProduto;
+import com.example.opet.aplicativomaiscadastros.Adapter.ProdutoAdapter;
+import com.example.opet.aplicativomaiscadastros.DAO.ProdutoDAO;
+import com.example.opet.aplicativomaiscadastros.Model.Produto;
+import com.example.opet.aplicativomaiscadastros.R;
+
+import java.util.List;
+
 /**
  * Created by opet on 16/05/2018.
  */
 
-public class ActivityListarProdutos {
+public class ActivityListarProdutos extends Activity {
+
+    private ListView listaProdutos;
+    private ProdutoAdapter myAdapter;
+    ProdutoDAO produtoDAO;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar_produtos);
+
+        carregarElementos();
+    }
+
+    public void carregarElementos(){
+        listaProdutos = (ListView) findViewById(R.id.listProdutos);
+        produtoDAO = new ProdutoDAO(this);
+        List<Produto> produtos = produtoDAO.carregaDadosLista();
+        myAdapter = new ProdutoAdapter(this, R.layout.item_produto,produtos);
+        listaProdutos.setAdapter(myAdapter);
+        listaProdutos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Produto produto = (Produto)parent.getItemAtPosition(position);
+                Intent atualizarIntent = new Intent(ActivityListarProdutos.this,ActivityAtualizarProduto.class);
+                atualizarIntent.putExtra("ID_PRODUTO",produto.getIdProduto());
+                startActivity(atualizarIntent);
+            }
+        });
+    }
 }
