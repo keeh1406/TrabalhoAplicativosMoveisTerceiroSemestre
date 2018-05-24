70c716458997c7c5d21f85c70f96d63190d0e272
opet <opet@Academico.edu>
2018-05-24 09:40:09 -0300

    24/05/2018 09h40

--- app/src/main/java/com/example/opet/aplicativomaiscadastros/ActivityListar/ActivityListarVendas.java
+++ app/src/main/java/com/example/opet/aplicativomaiscadastros/ActivityListar/ActivityListarVendas.java
@@ -1,8 +1,51 @@
 package com.example.opet.aplicativomaiscadastros.ActivityListar;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.example.opet.aplicativomaiscadastros.ActivityAtualizar.ActivityAtualizarVenda;
+import com.example.opet.aplicativomaiscadastros.Adapter.VendaAdapter;
+import com.example.opet.aplicativomaiscadastros.DAO.VendaDAO;
+import com.example.opet.aplicativomaiscadastros.Model.Venda;
+import com.example.opet.aplicativomaiscadastros.R;
+
+import java.util.List;
+
 /**
  * Created by opet on 23/05/2018.
  */
 
-public class ActivityListarVendas {
+public class ActivityListarVendas extends Activity {
+
+    private ListView listaVendas;
+    private VendaAdapter myAdapter;
+    VendaDAO vendaDAO;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar_vendas);
+
+        carregarElementos();
+    }
+
+    public void carregarElementos(){
+        listaVendas = (ListView) findViewById(R.id.listVendas);
+        vendaDAO = new VendaDAO(this);
+        List<Venda> vendas = vendaDAO.carregaDadosLista();
+        myAdapter = new VendaAdapter(this, R.layout.item_venda,vendas);
+        listaVendas.setAdapter(myAdapter);
+        listaVendas.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Venda venda = (Venda)parent.getItemAtPosition(position);
+                Intent atualizarIntent = new Intent(ActivityListarVendas.this,ActivityAtualizarVenda.class);
+                atualizarIntent.putExtra("ID_VENDA",venda.getIdVenda());
+                startActivity(atualizarIntent);
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/opet/aplicativomaiscadastros/MainActivity.java
+++ app/src/main/java/com/example/opet/aplicativomaiscadastros/MainActivity.java
@@ -1,13 +1,100 @@
 package com.example.opet.aplicativomaiscadastros;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.opet.aplicativomaiscadastros.ActivityCadastrar.ActivityCadastrarCliente;
+import com.example.opet.aplicativomaiscadastros.ActivityCadastrar.ActivityCadastrarFornecedor;
+import com.example.opet.aplicativomaiscadastros.ActivityCadastrar.ActivityCadastrarFuncionario;
+import com.example.opet.aplicativomaiscadastros.ActivityCadastrar.ActivityCadastrarLoja;
+import com.example.opet.aplicativomaiscadastros.ActivityCadastrar.ActivityCadastrarMarca;
+import com.example.opet.aplicativomaiscadastros.ActivityCadastrar.ActivityCadastrarProduto;
+import com.example.opet.aplicativomaiscadastros.ActivityCadastrar.ActivityCadastrarSetor;
+import com.example.opet.aplicativomaiscadastros.ActivityCadastrar.ActivityCadastrarVenda;
+import com.example.opet.aplicativomaiscadastros.ActivityListar.ActivityListarClientes;
+import com.example.opet.aplicativomaiscadastros.ActivityListar.ActivityListarFornecedores;
+import com.example.opet.aplicativomaiscadastros.ActivityListar.ActivityListarFuncionarios;
+import com.example.opet.aplicativomaiscadastros.ActivityListar.ActivityListarLojas;
+import com.example.opet.aplicativomaiscadastros.ActivityListar.ActivityListarMarcas;
+import com.example.opet.aplicativomaiscadastros.ActivityListar.ActivityListarProdutos;
+import com.example.opet.aplicativomaiscadastros.ActivityListar.ActivityListarSetores;
+import com.example.opet.aplicativomaiscadastros.ActivityListar.ActivityListarVendas;
+
+public class MainActivity extends Activity {
+
+    private TextView textWelcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textWelcome = (TextView) findViewById(R.id.textWelcome);
+
+        Intent resultado = getIntent();
+
+        textWelcome.setText("Olá!");
+    }
+
+    public void carregaItemMenu(View v){
+        switch (v.getId()) {
+            case R.id.btnInserirCliente:
+                carregarIntent(ActivityCadastrarCliente.class);
+                break;
+            case R.id.btnInserirFuncionario:
+                carregarIntent(ActivityCadastrarFuncionario.class);
+                break;
+            case R.id.btnInserirFornecedor:
+                carregarIntent(ActivityCadastrarFornecedor.class);
+                break;
+            case R.id.btnInserirLoja:
+                carregarIntent(ActivityCadastrarLoja.class);
+                break;
+            case R.id.btnInserirMarca:
+                carregarIntent(ActivityCadastrarMarca.class);
+                break;
+            case R.id.btnInserirSetor:
+                carregarIntent(ActivityCadastrarSetor.class);
+                break;
+            case R.id.btnInserirProduto:
+                carregarIntent(ActivityCadastrarProduto.class);
+                break;
+            case R.id.btnInserirVenda:
+                carregarIntent(ActivityCadastrarVenda.class);
+                break;
+            case R.id.btnListarClientes:
+                carregarIntent(ActivityListarClientes.class);
+                break;
+            case R.id.btnListarFuncionarios:
+                carregarIntent(ActivityListarFuncionarios.class);
+                break;
+            case R.id.btnListarFornecedores:
+                carregarIntent(ActivityListarFornecedores.class);
+                break;
+            case R.id.btnListarLojas:
+                carregarIntent(ActivityListarLojas.class);
+                break;
+            case R.id.btnListarMarcas:
+                carregarIntent(ActivityListarMarcas.class);
+                break;
+            case R.id.btnListarSetores:
+                carregarIntent(ActivityListarSetores.class);
+                break;
+            case R.id.btnListarProdutos:
+                carregarIntent(ActivityListarProdutos.class);
+                break;
+            case R.id.btnListarVendas:
+                carregarIntent(ActivityListarVendas.class);
+                break;
+        }
+    }
+
+    private void carregarIntent(Class classe){
+        Intent intent = new Intent(MainActivity.this,classe);
+        startActivity(intent);
     }
 }
