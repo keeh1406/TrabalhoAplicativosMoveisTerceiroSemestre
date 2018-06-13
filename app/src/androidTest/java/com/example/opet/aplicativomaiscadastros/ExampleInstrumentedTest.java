dc06b0ac41e54dcf5eb80e9fe2432ce24497dee9
opet <opet@Academico.edu>
2018-06-13 11:12:39 -0300

    13/06/2018 11h12

--- app/src/androidTest/java/com/example/opet/aplicativomaiscadastros/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/opet/aplicativomaiscadastros/ExampleInstrumentedTest.java
@@ -4,6 +4,10 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.example.opet.aplicativomaiscadastros.DAO.ClienteDAO;
+import com.example.opet.aplicativomaiscadastros.Factory.DataBaseFactory;
+import com.example.opet.aplicativomaiscadastros.Model.Cliente;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -22,5 +26,20 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.example.opet.aplicativomaiscadastros", appContext.getPackageName());
+    }
+
+    @Test
+    public void testeInsertBanco() throws Exception {
+        Context appContext = InstrumentationRegistry.getTargetContext();
+        ClienteDAO clienteDAO = new ClienteDAO(appContext);
+        Cliente cliente = new Cliente();
+        cliente.setNomeCliente("Kethllyn");
+        cliente.setCPFCliente(06111821903);
+        assertTrue(clienteDAO.insereDado(cliente) > 0);
+        Cliente resultado = clienteDAO.carregaClientePorID(3);
+        assertEquals(cliente, cliente);
+        resultado.setNomeCliente(String.valueOf(clienteDAO));
+        assertTrue(clienteDAO.atualizaCliente(resultado) > 0);
+        assertTrue(clienteDAO.deletaRegistro(resultado) > 0);
     }
 }
